5422cd18e343d91f737f974b09868c352c67aa5c
generalfrazer <justforgptspjimr@gmail.com>
2024-11-27 13:40:35 +0530

    Updated MongoDB connection, added .gitignore, and updated entry point

--- app.js
+++ app.js
@@ -9,16 +9,10 @@ const PORT = 3000;
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Connected to MongoDB...');
-})
-.catch(err => {
-  console.error('Could not connect to MongoDB...', err);
-});
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
 
 // Connect to MongoDB using Mongoose
 mongoose.connect(process.env.MONGODB_URI, {
