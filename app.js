bc9b2b9045c2c1f54911cda486a7dcf813f8632e
generalfrazer <justforgptspjimr@gmail.com>
2024-11-27 14:36:55 +0530

    Removed deprecated Mongoose options from app.js

--- app.js
+++ app.js
@@ -10,12 +10,9 @@ const PORT = 3000;
 const mongoose = require('mongoose');
 
 // Connect to MongoDB using Mongoose
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB...'))
-.catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
 
 // Define the Log schema
 const logSchema = new mongoose.Schema({
